d8d586d8c31767aaba9a1ca524fd0cfd37c3db23
Yuriy64M <Vasy64M@mail.ru>
2021-03-12 21:04:54 +0400

    12.03.21 последнее

--- js/scriptmodal.js
+++ js/scriptmodal.js
@@ -1,24 +1,11 @@
-
-
 const  searchHotelButton = document.querySelector(".search-hotel");
 const  modalWindow = document.querySelector(".modal");
-const  animationModalTwo = document.querySelector(".animation-modal-two");
-const  searchHotelOpen = document.querySelector(".search-hotel-open");
 const  searchHotelClicked = document.querySelector(".search-hotel-clicked");
 
 searchHotelButton.addEventListener ("click", function () {
 
-
-  if (searchHotelClicked) {
-    modalWindow.classList.remove ("search-hotel-clicked");
     modalWindow.classList.toggle ("animation-modal-two");
-    console.log("ffffffff");
-  } else
 
-  if (animationModalTwo || searchHotelOpen) {
-    modalWindow.classList.remove ("animation-modal-two");
-    modalWindow.classList.remove ("search-hotel-open");
     modalWindow.classList.toggle ("search-hotel-clicked");
-    console.log("ooooooooooo");
-  }
+
 });
